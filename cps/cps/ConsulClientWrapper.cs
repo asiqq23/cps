688b774154e1a2e48b5387e0cf7a9e2a0f90f7e0
Adrian Zawadzki <adrian.zawadzki@fabres.pl>
2017-08-04 17:19:26 +0200

    register routes

---
+++ cps/cps/ConsulClientWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Consul;
+
+
+namespace cps
+{
+    public class ConsulClientWrapper
+    {
+        public async Task RegisterRoutes()
+        {
+            using (var client = GetClient())
+            {
+                var putPair = new KVPair("cps")
+                {
+                    Value = Encoding.UTF8.GetBytes("/api/values")
+                };
+
+                await client.KV.Put(putPair);
+            }
+        }
+
+        private ConsulClient GetClient()
+        {
+            return new ConsulClient(configuration =>
+            {
+                configuration.Address = new Uri("http://192.168.44.117:8500");
+            });
+        }
+
+    }
+}
